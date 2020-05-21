128556011038f26ed7d5b46edac52059ef0a9d4a
David MacPhail <david.macphail@icloud.com>
2020-05-21 21:17:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   object.key = 'value';
   return obj
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return obj
+}

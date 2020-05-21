46355ba24d34f7651caeac5e188cb8b49f3dba3e
David MacPhail <david.macphail@icloud.com>
2020-05-21 21:09:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   Object.assign({}, object, {key: ['value']})
   return obj
 }
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object.key = ['value']
+  retirn obj
+}
